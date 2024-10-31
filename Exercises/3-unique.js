0457005cbee7fdb55cef28ee3e7998219106efec
DimaBohdan <bohdan777.dmytro@gmail.com>
2024-10-31 20:17:13 +0200

    tasks completed

--- Exercises/3-unique.js
+++ Exercises/3-unique.js
@@ -3,6 +3,13 @@
 // Create and return a new array without duplicate elements
 // Don't modify initial array
 
-const unique = (array) => [];
+const unique = (array) => {
+  const res = [];
+  for (const el of array) {
+    if (!res.includes(el))
+      res.push(el);
+  }
+  return res;
+};
 
 module.exports = { unique };
